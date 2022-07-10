c1cc82f7ca5287cb7163bea1d383c078878dcbe3
TkachevAnton <tkachevas.ne@gmail,com>
2022-07-10 16:46:20 +0300

    task01

--- task_00/Program.cs
+++ task_00/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write ("Write full number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int sqare = num*num;
+
+Console.Write ($"Sqare number {num}={sqare}");
---
+++ task_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine(a);
+        Console.WriteLine("Больше");
+            Console.WriteLine(b);
+}
+else
+{
+Console.WriteLine(b);
+Console.WriteLine("Больше");
+Console.WriteLine(a);
+}
