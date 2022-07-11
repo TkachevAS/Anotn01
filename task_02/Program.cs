cbcbba2e316a1ed8ecfedaedb8e9fbc9f131f60b
TkachevAnton <tkachevas.ne@gmail,com>
2022-07-11 16:25:02 +0300

    task02.v03

---
+++ task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число");
+int d=Convert.ToInt32(Console.ReadLine());
+int max=a;
+if(a>max) max=a;
+if(b>max) max=b;
+if(d>max) max=d;
+Console.WriteLine("max=");
+Console.WriteLine(max);
