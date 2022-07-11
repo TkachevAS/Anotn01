22c172d0c0613a372b05a5289abe30e9cc002785
TkachevAnton <tkachevas.ne@gmail,com>
2022-07-11 16:25:53 +0300

    task_03.v03

---
+++ task_03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число ");
+int x=Convert.ToInt32(Console.ReadLine());
+double x_2 = x % 2;
+if (x_2==0) 
+Console.WriteLine("число четное");
+else
+Console.WriteLine("Число нечетное");
+Console.ReadLine();
